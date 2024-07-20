47e5c05b6b3a7a042699f203d3ac66a912edee45
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-07-20 07:21:18 -0300

    adicionado a função ToString

--- main.cpp
+++ main.cpp
@@ -1,11 +1,37 @@
-#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <time.h>
+#include <math.h>
+
+
 using namespace std;
 
-int main()
+const int screen_width = 800;
+const int screen_height = 448;
+
+// use essa função pra converter int pra string
+template <typename T>
+
+// use essa função pra converter um valor pra string
+string ToString(T val)
+{
+    stringstream stream;
+    stream << val;
+    return stream.str();
+}
+
+
+int main(int argc, char *argv[])
 {
 setlocale(LC_ALL, "portuguese");
 
-cout << "olá mundo" << endl;
 
 return 0;
 }
